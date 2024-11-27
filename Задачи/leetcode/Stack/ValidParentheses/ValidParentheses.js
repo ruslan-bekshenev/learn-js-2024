dc846642e5cd11238f83a329688b4a6e9adc9907
roo <ruslancrtweb@yandex.ru>
2024-11-28 02:19:41 +0500

    valid parentheses

--- Задачи/leetcode/Stack/ValidParentheses/ValidParentheses.js
+++ Задачи/leetcode/Stack/ValidParentheses/ValidParentheses.js
@@ -22,3 +22,42 @@ var isValid = function(s) {
 
     return stack.length === 0
 };
+
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid2 = function(s) {
+    const stack = []
+    const map = {
+        "{" : "}",
+        "[" : "]",
+        "(" : ")"
+    }
+    // if (s.length < 2) {
+    //     return false
+    // }
+    for (let char of s) {
+        if (['{', '[', '('].includes(char)) {
+            stack.push(map[char])
+        }
+        else {
+            // const last = stack.pop()
+            // if (!last) {
+            //     return false
+            // }
+            // if (last === map[char]) {
+            //     return true
+            // } else if (last !== char) {
+            //     return false
+            // }
+
+            if (stack.pop() !== char) {
+                return false
+            }
+        }
+    }
+
+    return !stack.length
+};
