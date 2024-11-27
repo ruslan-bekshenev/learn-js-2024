8e741d55547b8927be4a697c83fe4292ed81a507
roo <ruslancrtweb@yandex.ru>
2024-11-28 01:49:44 +0500

    бинарный поиск

--- Алгоритмы/Бинарный поиск/index.js
+++ Алгоритмы/Бинарный поиск/index.js
@@ -1,23 +1,24 @@
 
-const array = [1,2,3,4,5,6,7,8,9,10]
-const element = 5
+const arr = [1,2,3,4,5,6,7,8,9,10]
+const target = 7
+let left = 0;
+let right = arr.length - 1;
+let result = -1;
+let i = 0
+while (left <= right) {
+  let mediana = Math.floor((left + right) / 2)
+  console.log(mediana)
+  if (target > arr[mediana]) {
+    left = mediana + 1
+  }
+  if (target < arr[mediana]) {
+    right = mediana - 1
+  }
 
-let resultIndex;
-let index = 0;
-let length = array.length
-let half = Math.round(array.length / 2)
-for (let i = index; i < length - half; i++) {
-  if (element > array[half - 1]) {
-    console.log('1')
-    index = length - half
-  } else if (element < array[half - 1]) {
-    index = half - 1
+  if (target === arr[mediana]) {
+    result = mediana
+    break
   }
-  if (array[index - 1] === element) {
-    resultIndex = index - 1
-    break;
-  }
-  half = Math.round(half / 2)
 }
 
-console.log(resultIndex)
+console.log(result)
