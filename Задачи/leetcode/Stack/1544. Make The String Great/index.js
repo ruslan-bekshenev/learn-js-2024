bef1f65811b2e0b4e5c0d4bc0928cbe191d67132
roo <ruslancrtweb@yandex.ru>
2024-12-05 13:34:33 +0500

    1544. Make The String Great

---
+++ Задачи/leetcode/Stack/1544. Make The String Great/index.js
@@ -0,0 +1,16 @@
+function makeGood(s) {
+    const stack = []
+
+    for (let i = 0; i < s.length; i++) {
+        const prev= stack[stack.length-1]
+        const cur=s[i]
+
+        if(prev && prev!==cur && prev.toUpperCase()==cur.toUpperCase()){
+            stack.pop()
+        } else {
+            stack.push(s[i])
+        }
+    }
+
+    return stack.join('')
+};
